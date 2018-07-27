9b4b1e9bcb849b817ba37a1925125b34c0eb307a
tkingless <t.kingless@yahoo.com>
2018-07-27 03:50:25 +0000

    a few start up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+  ##/ get this right: get 'welcome/', to redirect('welcome#index')
+  get 'welcome' => redirect('welcome/index')
+  resources :articles
+  root :to => 'welcome#index'
+  get '*path' => redirect('/')
 end
