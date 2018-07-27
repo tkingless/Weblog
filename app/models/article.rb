b9710bb4a0824ccd306a85ba695c2d4f75345ff0
tkingless <t.kingless@yahoo.com>
2018-07-27 09:36:57 +0000

    added comment and missed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
+##/ TODO how to clear dynamic data in db
+##/ TODO how to separate dynamic and static data in db
+
 class Article < ApplicationRecord
   ##/How to use ActiveRecord? here the model attribute not auto declared
+  ##/Validation, https://guides.rubyonrails.org/active_record_validations.html:
+  validates :title, presence: true, length: { minimum: 5 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+#https://guides.rubyonrails.org/routing.html#direct-routes
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'welcome/index'
